db4eb072cac69bf7f428042fe41ff554f7e794cd
Pascal Kirchner <pascal.kirchner@web.de>
2019-06-05 16:10:40 +0200

    Added documentation. Added functionality for checking paths, setting a root path and transforming lois to paths. Added error classes.

--- labinform/datasafe/datasafe.py
+++ labinform/datasafe/datasafe.py
@@ -1,20 +1,162 @@
+import os
+
+
+class Error(Exception):
+    """
+    Base class for exceptions in this module.
+    """
+    pass
+
+
+class NoSuchDirectoryError(Error):
+    """
+    Raised when an invalid path is set.
+    """
+    pass
+
+
+class IncorrectLoiError(Error):
+    """
+    Raised when an incorrect loi is provided.
+    """
+    pass
+
+
 class Datasafe:
+    """Data handler for moving data in the context of a
+    datasafe.
+
+    The operations performed include generation of a directory structure,
+    storing data in and retrieving data from these directories as well
+    verifying the integrity of and providing general information about the
+    data stored.
+    """
+    def __init__(self):
+        self.path = ""
+
+    def setPath(self, path=""):
+        """Sets the path of the datasafe's toplevel directory.
+
+        :param path: The path that should be set as the instance attribute.
+        """
+        print(path)
+        if not self.verifyPath(path):
+            raise NoSuchDirectoryError
+        self.path = path
+
+    def verifyPath(self, path=""):
+        """Verifies if a path is correct.
+
+        :param path: path that should be checked
+        :return: result of the path check
+        """
+        return os.path.isdir(path)
+
+    def verifyOwnPath(self):
+        """Verifies if the path set as instance attribute is a correct path.
+
+        :return: result of the path check
+        """
+        return self.verifyPath(self.path)
+
+    def loiToPath(self, loi=""):
+        """Retrieves the a file's datasafe directory path from the
+        corresponding loi.
+
+        :param loi: loi for which the path should be retrieved
+        :return: path of the file
+        """
+        path = self.path
+        loi_parts = loi.split("/")
+        if len(loi_parts) != 7:
+            raise IncorrectLoiError
+        loi_parts_useful = loi_parts[2:]
+        for part in loi_parts_useful:
+            path += "/"
+            path += part
+        return path
+
+    def addDirectory(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     def generate(self, experiment="", sample_id=""):
+        """Generate directory structure and return identifier.
+
+        This method will verify to what extent the relevant directory structure
+        is present and create directories as required. In this context the
+        measurement number for a given sample is - in case of consecutive
+        measurements - automatically increased.
+
+        Finally the method will return a unique identifier for the respective
+        measurement and sample, including the directory path.
+
+        :param experiment: type of experiment performed, e.g. cwepr
+        :param sample_id: unique identifier for the sample measured
+        :return: unique loi including the information provided
+        """
+        loi_basic = "42.1001/ds/"
+
         return "42.1001/ds/cwepr/sa42/01/data/raw"
 
     def push(self, data="", loi=""):
+        """Move data inside the datasafe.
+
+        Before moving the existence of the target directory (as specified in
+        the loi) as well as its emptiness are verified.
+
+        :param data: data (file) to be moved
+        :param loi: unique identifier providing the target directory
+        """
         pass
 
     def pull(self, loi=""):
+        """Retrieve data from the datasafe.
+
+        Retrieves data from the datasafe if present at the target directory
+        (as specified in the loi), raises an exception otherwise.
+
+        :param loi: identifier for the data (file) to be retrieved
+        :return: retrieved data
+        """
         return str()
 
     def index(self, loi=""):
+        """Retrieve background information from the datasafe.
+
+        Retrieves background information (manifest.yaml file) from the datasafe
+        if present at the target directory (as specified in the loi), raises
+        an exception otherwise.
+
+        :param loi: identifier for the data (file) for which the background
+         information should be retrieved
+        :return: background information (manifest.yaml) as dict
+        """
         return dict()
 
     def checksum(self, loi=""):
+        """Create a checksum (MD5) for a file in the datasafe.
+
+        Creates a checksum for a file in the datasafe if present at the target
+        directory (as specified in the loi), raises an exception otherwise.
+
+        :param loi: identifier for the data (file) for which the checksum
+        should be created
+        :return: checksum (MD5)
+        """
         return str()
 
-    def moveto(self):
-        pass
+    def moveto(self, data="", experiment="", sample=""):
+        """Wrapper function including all operations necessary for moving data
+        to the datasafe.
+
+        Calls :method:`generate` to generate a directory structure if
+        necessary and creates a local checksum of the file to be moved.
+        Then moves the file to the datasafe, creates another checksum.
+        The two checksums are compared and the result of the comparation is
+        returned.
+
+        :return: result of the checksum comparation
+        """
+        return True
 
--- tests/datasafe/test_datasafe.py
+++ tests/datasafe/test_datasafe.py
@@ -1,8 +1,8 @@
 import unittest
+import tempfile
+import os
 
 import labinform.datasafe.datasafe as datasafe
-#import aspecd
-#from cwepr import dataset
 
 
 class TestDatasafe(unittest.TestCase):
@@ -67,11 +67,160 @@ class TestDatasafe(unittest.TestCase):
         self.assertTrue(hasattr(self.datasafe, 'moveto'))
         self.assertTrue(callable(self.datasafe.moveto))
 
+    def test_call_moveto_with_parameters(self):
+        self.datasafe.moveto("", "cwepr", "sa42")
+
+    def test_moveto_returns_bool(self):
+        worked = self.datasafe.moveto("", "cwepr", "sa42")
+        self.assertEqual(bool, type(worked))
+
+    def test_has_set_path_method(self):
+        self.assertTrue(hasattr(self.datasafe, 'setPath'))
+        self.assertTrue(callable(self.datasafe.setPath))
+
+    def test_call_set_path_with_parameters(self):
+        try:
+            self.datasafe.setPath("")
+        except datasafe.NoSuchDirectoryError:
+            pass
+
+    def test_has_verify_path_method(self):
+        self.assertTrue(hasattr(self.datasafe, 'verifyPath'))
+        self.assertTrue(callable(self.datasafe.verifyPath))
+
+    def test_call_verify_path_with_parameters(self):
+        self.datasafe.verifyPath("")
+
+    def test_verify_path_returns_bool(self):
+        pathOkay = self.datasafe.verifyPath("")
+        self.assertEqual(bool, type(pathOkay))
+
+    def test_has_verify_own_path(self):
+        self.assertTrue(hasattr(self.datasafe, 'verifyOwnPath'))
+        self.assertTrue(callable(self.datasafe.verifyOwnPath))
+
+    def test_verify_own_path_returns_bool(self):
+        pathOkay = self.datasafe.verifyOwnPath()
+        self.assertEqual(bool, type(pathOkay))
+
+    def test_has_loi_to_path_method(self):
+        self.assertTrue(hasattr(self.datasafe, 'loiToPath'))
+        self.assertTrue(callable(self.datasafe.loiToPath))
+
+    def test_call_loi_to_path_with_parameters(self):
+        self.datasafe.loiToPath("42.1001/ds/cwepr/sa42/01/data/raw")
+
+    def test_has_add_directory_method(self):
+        self.assertTrue(hasattr(self.datasafe, 'addDirectory'))
+        self.assertTrue(callable(self.datasafe.addDirectory))
+
+    def test_call_add_directory_with_parameters(self):
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        datasafe_path = temp_dir.name
+        self.datasafe.setPath(datasafe_path)
+        self.datasafe.addDirectory(self.datasafe.path + "cwepr")
+
+    #def test_has_dir_empty_method(self):
+
 
 class TestGenerate(unittest.TestCase):
     def setUp(self):
         self.datasafe = datasafe.Datasafe()
         #self.path =
+
+
+class TestSetPath(unittest.TestCase):
+    def setUp(self):
+        self.datasafe = datasafe.Datasafe()
+
+    def tearDown(self):
+        pass
+
+    def test_set_path(self):
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        datasafe_path = temp_dir.name
+        self.datasafe.setPath(datasafe_path)
+        self.assertEqual(True, hasattr(self.datasafe, 'path'))
+        self.assertEqual(str, type(self.datasafe.path))
+
+    def test_set_path_raises_error_for_incorrect_path(self):
+        with self.assertRaises(datasafe.NoSuchDirectoryError):
+            self.datasafe.setPath("")
+
+
+class TestVerifyPath(unittest.TestCase):
+    def setUp(self):
+        self.datasafe = datasafe.Datasafe()
+
+    def tearDown(self):
+        pass
+
+    def test_verify_path_returns_false_for_incorrect_path(self):
+        pathOkay = self.datasafe.verifyPath("")
+        self.assertEqual(False, pathOkay)
+
+    def test_verify_path_returns_true_for_correct_path(self):
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        datasafe_path = temp_dir.name
+        pathOkay = self.datasafe.verifyPath(datasafe_path)
+        self.assertEqual(True, pathOkay)
+
+    def test_verify_own_path_returns_false_when_path_not_set(self):
+        pathOkay = self.datasafe.verifyOwnPath()
+        self.assertEqual(False, pathOkay)
+
+    def test_verify_own_path_returns_true_when_path_is_set(self):
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        datasafe_path = temp_dir.name
+        self.datasafe.setPath(datasafe_path)
+        pathOkay = self.datasafe.verifyOwnPath()
+        self.assertEqual(True, pathOkay)
+
+
+class TestLoiToPath(unittest.TestCase):
+    def setUp(self):
+        self.datasafe = datasafe.Datasafe()
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        self.datasafe_path = temp_dir.name
+        self.datasafe.setPath(self.datasafe_path)
+
+    def tearDown(self):
+        pass
+
+    def test_correct_path_from_loi(self):
+        path_correct = self.datasafe_path + "/cwepr/sa42/01/data/raw"
+        path_experimental = self.datasafe.loiToPath("42.1001/ds/cwepr/sa42/01/data/raw")
+        self.assertEqual(path_correct, path_experimental)
+
+    def test_loi_to_path_raises_error_for_incorrect_loi(self):
+        with self.assertRaises(datasafe.IncorrectLoiError):
+            self.datasafe.loiToPath("42.1001//raw")
+
+
+class TestAddDirectory(unittest.TestCase):
+    def setUp(self):
+        self.datasafe = datasafe.Datasafe()
+        target_directory = "/home/kirchner/nas/Praktikum/datasafe-test"
+        temp_dir = tempfile.TemporaryDirectory(dir=target_directory)
+        self.datasafe_path = temp_dir.name
+        self.datasafe.setPath(self.datasafe_path)
+
+    def tearDown(self):
+        pass
+
+    def test_directory_is_added(self):
+        path_complete = self.datasafe.path + "cwepr"
+        self.datasafe.addDirectory(path_complete)
+        self.assertTrue(os.path.isdir(path_complete))
+
+
+
+
 
 
 
